6d52bca742a8ec6e1f723c79537f07dd51b8fef6
meinfr13nd <sarahsamora@mail.com>
2020-10-28 21:43:08 -0700

    unborked a little

--- cloudhaven/client/src/app.js
+++ cloudhaven/client/src/app.js
@@ -1,7 +1,10 @@
 import React, { useEffect, useState } from 'react';
+import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 
 import './site.css';
+import makeStyles from "@material-ui/core/styles/makeStyles";
+import Input from "@material-ui/core/Input";
 
 export default function App() {
   const [response, setResponse] = useState('');
@@ -12,7 +15,16 @@ export default function App() {
     callApi()
       .then(resp => setResponse(resp.express))
       .catch(err => console.log(err));
-  })
+  });
+
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      '& .MuiTextField-root': {
+        margin: theme.spacing(1),
+        width: '25ch',
+      },
+    },
+  }));
 
   const callApi = async () => {
     const response = await fetch('/api/hello');
@@ -26,6 +38,11 @@ export default function App() {
 
   const handleSubmit = async e => {
     e.preventDefault();
+
+    let nam = event.target.name;
+    let val = event.target.value;
+    this.setState({[nam]: val});
+
     const response = await fetch('/api/hello', {
       method: 'POST',
       headers: {
@@ -38,23 +55,38 @@ export default function App() {
     setResponseToPost(body);
   };
 
+  const classes = useStyles();
+
     return (
       <div className='centered'>
         <p>
           Welcome to CloudHaven
         </p>
         <p>{response}</p>
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit}  className={classes.root} noValidate autoComplete="off">
           <label htmlFor='FormInput'>
             <strong>Post to Server: </strong>
           </label>
-          <input
-            id='FormInput'
-            type="text"
-            value={post}
-            onChange={e => setPost(e.target.value)}
-          />
-          <button type="submit">Submit</button>
+          <div>
+            <TextField
+                id="filled-basic"
+                label="Username"
+                variant="filled"
+                required={true}
+                onChange={user => setPost(user.target.value)}
+            />
+          </div>
+          <div>
+            <TextField
+                id="filled-basic"
+                label="Password"
+                variant="filled"
+                required={true}
+                onChange={pass => setPost(pass.target.value)}
+            />
+          </div>
+
+          <Button type="submit" variant="contained" color="primary">Submit</Button>
         </form>
         <p>{responseToPost}</p>
       </div>
