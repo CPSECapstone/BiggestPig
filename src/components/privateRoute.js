3bdd0a9cf213521dd1d457c418159a869ffe0a58
Sarah Samora <sarahsamora@mail.com>
2020-11-11 16:21:26 -0800

    hardcoded isAuthenticated

--- src/components/privateRoute.js
+++ src/components/privateRoute.js
@@ -6,7 +6,8 @@ import { useAuth } from '../contexts/authorize.js';
 export default function PrivateRoute({ component: Component, ...rest }) {
    const currAuth = useAuth();
 
-   const isAuthenticated = typeof currAuth != undefined && currAuth['auth'] !== undefined && currAuth['auth'] !== null;
+   // const isAuthenticated = typeof currAuth != undefined && currAuth['auth'] !== undefined && currAuth['auth'] !== null;
+   const isAuthenticated = true;
 
    return (
       <Route {...rest}
