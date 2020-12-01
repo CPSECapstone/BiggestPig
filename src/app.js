1ffe083536161e395d61e7c2364675e79ed8ced5
Justin Evans <17054595+nauticalmonkey@users.noreply.github.com>
2020-11-30 22:51:19 -0800

    addind comments

--- src/app.js
+++ src/app.js
@@ -1,18 +1,15 @@
 import React, {useState} from 'react';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-
 import {Provider} from './components/providers.js';
 import {Header} from './components/defaultComponents';
-
 import Home from './pages/home';
 import Demo2 from './pages/demo2';
 import Login from './pages/login';
 import FormPropsTextFields from './pages/demo';
-
 import PrivateRoute from './components/privateRoute.js';
 import PrivateComponent from './components/privateComponent.js';
-
 import {AuthContext} from './contexts/authorize.js';
+
 
 export default function App() {
   const currAuth = JSON.parse(sessionStorage.getItem('auth'));
