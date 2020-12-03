e48a73e96df24b2bc6e8f9e680613075e54b7668
Evan Rozhon <erozhon@users.noreply.github.com>
2020-12-02 21:55:21 -0800

    button update

--- src/components/button.js
+++ src/components/button.js
@@ -3,7 +3,7 @@ import Button from '@material-ui/core/Button';
 // TODO incorporate target with cookies
 //  add more fields to customize buttons
 export default function buttonGenerator(props) {
-  return (<Button onClick=
+  return (<Button variant='outlined' onClick=
     {() => {
       alert('clicked');
     }
