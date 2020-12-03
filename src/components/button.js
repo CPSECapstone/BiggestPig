9be5b4623dcb762e2723eeaf538345f863bb12c3
Evan Rozhon <erozhon@users.noreply.github.com>
2020-12-02 17:14:49 -0800

    button parsing and updated form.js

---
+++ src/components/button.js
@@ -0,0 +1,11 @@
+import Button from '@material-ui/core/Button';
+
+// TODO incorporate target with cookies
+//  add more fields to customize buttons
+export default function buttonGenerator(props) {
+  return (<Button onClick=
+    {() => {
+      alert('clicked');
+    }
+    }>props.action</Button>);
+}
--- src/components/form.js
+++ src/components/form.js
@@ -1,5 +1,5 @@
 import React, { } from 'react';
-
+import buttonGenerator from './button';
 import TextField from '@material-ui/core/TextField';
 
 // THIS FUNCTION TAKES IN SOME STUFF (from an API that we defined)
@@ -13,34 +13,43 @@ import TextField from '@material-ui/core/TextField';
 export default function formGenerator(props) {
   const formId = props.formID;
   const childrenArray = [];
+
   for (const i in props) {
     if (i.startsWith('column')) {
       childrenArray.push(props.[i].children);
     }
   }
-  console.log('childrenfound');
-  console.log(childrenArray);
 
   function renderDetails(information) {
-    return (
-      <div>
-        <TextField
-          id={'filled-basic'}
-          label={information.label}
-          fullWidth
-          variant="filled"
-          defaultValue={information.validation}
-        />
-        <p></p>
-      </div>
-    );
+    console.log(information.label);
+    if (typeof information.label !== 'undefined') {
+      return (
+        <div>
+          <TextField
+            id={'filled-basic'}
+            label={information.label}
+            fullWidth
+            variant="filled"
+            defaultValue={information.validation}
+          />
+          <p></p>
+        </div>
+      );
+    } else {
+      console.log('abcxyz');
+      return information;
+    }
   }
 
   function renderItems(fieldVar) {
     const f = [];
     if (fieldVar) {
       for (const i in fieldVar) {
-        f.push(fieldVar.[i]);
+        if (i !== 'button') {
+          f.push(fieldVar.[i]);
+        } else {
+          f.push(buttonGenerator(fieldVar.[i]));
+        }
       }
     }
 
