262a08a1c6cb2f32e9249e663df7e111227bc43d
nauticalmonkey <17054595+nauticalmonkey@users.noreply.github.com>
2021-04-21 19:47:01 -0700

    set up java file structure for tests
    Looking through java code didnt see much that can be currently tested or that wont be changed soon with functional app

---
+++ tests/javatests.java
@@ -0,0 +1,9 @@
+/*
+Junit test to test the java version of code implmented within cloudhaven
+This can be reviewed at https://www.tutorialspoint.com/junit/junit_test_framework.htm
+*/
+import junit.*;
+import junit_test_framework.*;
+import org.junit.runners.*;
+
+//current faker class awaiting update
