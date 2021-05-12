2ce099930b064d2c30b4b5dadbb20b5e86fd31d7
Justin Evans <17054595+nauticalmonkey@users.noreply.github.com>
2021-05-11 20:04:56 -0700

    WLEC stuff and some updates to python

--- apps/pythonVendor/dummy.py
+++ apps/pythonVendor/dummy.py
@@ -5,6 +5,7 @@ app = Flask(__name__)
 CORS(app)
 
 @app.route('/hello/<name>')
+
 def hello(name):
    return jsonify(message='Hello %s!' % name)
 
@@ -43,14 +44,72 @@ def hello_world():
 
    column1["children"] = [c1Child0, c1Child1]
 
-   form["formID"] = "78b83774d3a907fbea42783d58a95204"
+   form["Title"] = "78b83774d3a907fbea42783d58a95204"
    form["component"] = "form"
    form["columns"] = [column0, column1]
-   return jsonify(authorization="6fa691d2c07bea6ae78b7022c80e9d20",
-                  target="42aefbae01d2dfd981f7da7d823d689e",
+   return jsonify(
+                  Vendorid = "SnakeSnakeImASnake",
+                  VendorAuth="6fa691d2c07bea6ae78b7022c80e9d20",
                   components=[
                      form
                   ])
+
+
+def interceptmessage(message):
+   if message == {"VendorAuth":"6fa691d2c07bea6ae78b7022c80e9d20","Vendorid":"SnakeSnakeImASnake","components":[{"Title":"78b83774d3a907fbea42783d58a95204","columns":[{"children":[{"component":"textFormField","label":"Full Name","required":"true","validation":"[a-fA-F0-9]"},{"component":"textFormField","label":"Address","required":"true","validation":"[a-fA-F0-9]"}]},{"children":[{"component":"textFormField","label":"Phone Number","required":"true","validation":"[a-fA-F0-9]"},{"action":"Submit","component":"button","target":"78b83774d3a907fbea42783d58a95204"}]}],"component":"form"}]}:
+      logging.info('just testing and pasted')
+      return message
+   else: 
+      logging.info('just testing and FAILED')
+      return 'test failed'
+
+
+@app.route('/test')
+def test_app():
+   logging.info('just testing')
+   form = dict()
+
+   column0 = dict()
+   c0Child0 = dict()
+   c0Child1 = dict()
+   c0Child0['component'] = "textFormField"
+   c0Child0['label'] = "Full Name"
+   c0Child0['required'] = "true"
+   c0Child0['validation'] = "[a-fA-F0-9]"
+
+   c0Child1['component'] = "textFormField"
+   c0Child1['label'] = "Address"
+   c0Child1['required'] = "true"
+   c0Child1['validation'] = "[a-fA-F0-9]"
+
+   column0["children"] = [c0Child0, c0Child1]
+
+
+   column1 = dict()
+   c1Child0 = dict()
+   c1Child1 = dict()
+   c1Child0['component'] = "textFormField"
+   c1Child0['label'] = "Phone Number"
+   c1Child0['required'] = "true"
+   c1Child0['validation'] = "[a-fA-F0-9]"
+
+   c1Child1['component'] = "button"
+   c1Child1['action'] = "Submit"
+   c1Child1['target'] = "78b83774d3a907fbea42783d58a95204"
+
+   column1["children"] = [c1Child0, c1Child1]
+
+   form["Title"] = "78b83774d3a907fbea42783d58a95204"
+   form["component"] = "form"
+   form["columns"] = [column0, column1]
+   message= jsonify(
+                  Vendorid = "SnakeSnakeImASnake",
+                  VendorAuth="6fa691d2c07bea6ae78b7022c80e9d20",
+                  components=[
+                     form
+                  ])
+   interceptmessage(message)
+   return message
 
 
 
